05ba488319154d47af7cfda19e02726d5e747436
david <davidssesanga2020@gmail.com>
2023-01-26 12:10:37 -0500

    init

--- server/Routes/MessageRoute.js
+++ server/Routes/MessageRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addMessage, getMessages } from '../controllers/MessageController.js';
+import { addMessage, getMessages } from '../Controllers/MessageController.js';
 
 const router = express.Router();
 
